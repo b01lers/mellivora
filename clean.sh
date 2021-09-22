54eed776aed3a8884b6cd7c112c9eee68ecd71c3
novafacing <rowanbhart@gmail.com>
2021-09-22 18:49:48 -0400

    Remove several useless files

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm -rf certbot_certs
-rm -rf certbot_www
-rm -rf nginx_ssl
-
--- up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -a
-
-. config.env
-
-set +a
-
-docker-compose up --build --force-recreate
